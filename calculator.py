8800ac3d1d8ef32f05a4dc9b8a5cb912a41f09ab
prathamesh mardane <patumardane45@gmail.com>
2025-04-14 22:59:49 +0530

    any no

--- calculator.py
+++ calculator.py
@@ -28,7 +28,7 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-choice = input("Enter choice(1/2/3/4):")
+choice = input("Enter Any no. choice(1/2/3/4):")
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
